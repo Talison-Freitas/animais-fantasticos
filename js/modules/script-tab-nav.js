147cfffe4245619b062ceeaff3707b4cdd55e557
Talison-Freitas <Talisondefreitas@outlook.com>
2025-01-20 22:28:43 -0300

    refatoração do módulo tabnav

--- js/modules/script-tab-nav.js
+++ js/modules/script-tab-nav.js
@@ -1,16 +1,28 @@
 // Faz com que o texto se "adapte" a imagem baseado no click sobre elas.
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
-  const tabContent = document.querySelectorAll("[data-tab='content'] section");
-
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
-    tabContent[0].classList.add(tabContent[0].dataset.anima);
-
-    tabMenu.forEach((section, index) => {
+export default class TabNav {
+  constructor(imagem, texto) {
+    this.tabMenu = document.querySelectorAll(imagem);
+    this.tabContent = document.querySelectorAll(texto);
+    this.activeClass = "ativo";
+  }
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    const direcao = this.tabContent[index].dataset.anima;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+  addTabEvent() {
+    this.tabMenu.forEach((section, index) => {
       section.addEventListener("click", () => {
-        activeTab(index);
+        this.activeTab(index);
       });
     });
+  }
+  init() {
+    this.addTabEvent();
+    this.activeTab(0);
+    if (this.tabMenu.length && this.tabContent.length) {
+    }
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-import ScrollSuave from './modules/script-scroll-suave.js';
-import initAnimacaoScroll from './modules/script-scroll-animacao.js';
-import Accordion from './modules/script-accordion.js';
-import initScrollNav from './modules/script-nav-scroll.js';
-import initTabNav from './modules/script-tab-nav.js';
-import animacaoModal from './modules/script-animacao-modal.js';
-import initTooltip from './modules/script-tooltip.js';
-import initDropDownMenu from './modules/script-drop-down-menu.js';
-import initMenuMobile from './modules/script-menu-mobile.js';
-import initFuncionamento from './modules/script-funcionamento.js';
-import initFetchAnimais from './modules/script-fetch-animais.js';
-import initFetchBitcoin from './modules/script-fetch-bitcoin.js';
+import ScrollSuave from "./modules/script-scroll-suave.js";
+import initAnimacaoScroll from "./modules/script-scroll-animacao.js";
+import Accordion from "./modules/script-accordion.js";
+import initScrollNav from "./modules/script-nav-scroll.js";
+import TabNav from "./modules/script-tab-nav.js";
+import animacaoModal from "./modules/script-animacao-modal.js";
+import initTooltip from "./modules/script-tooltip.js";
+import initDropDownMenu from "./modules/script-drop-down-menu.js";
+import initMenuMobile from "./modules/script-menu-mobile.js";
+import initFuncionamento from "./modules/script-funcionamento.js";
+import initFetchAnimais from "./modules/script-fetch-animais.js";
+import initFetchBitcoin from "./modules/script-fetch-bitcoin.js";
 
 const scrollSuave = new ScrollSuave("[data-interface='menu'] a[href^='#']");
 scrollSuave.init();
 
 const accordion = new Accordion("[data-anima='accordion'] dt");
 accordion.init();
+
+const tabNav = new TabNav(
+  "[data-tab='menu'] li",
+  "[data-tab='content'] section"
+);
+tabNav.init();
+
 initAnimacaoScroll();
 initScrollNav();
-initTabNav();
 animacaoModal();
 initTooltip();
 initDropDownMenu();
@@ -27,4 +33,3 @@ initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
 
-// "[data-interface='menu'] a[href^='#']"
