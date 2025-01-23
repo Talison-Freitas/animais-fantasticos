a845b488b50211cd69f279d601ac55e9aa9e2105
Talison-Freitas <Talisondefreitas@outlook.com>
2025-01-23 15:26:56 -0300

    Refatoração do módulo fetch-bitcoin.

--- js/modules/script-fetch-bitcoin.js
+++ js/modules/script-fetch-bitcoin.js
@@ -1,14 +1,22 @@
-export default function initFetchBitcoin() {
-  const displayBtc = document.querySelector(".btc-preco");
-  async function fetchBitcoin(url) {
+export default class FetchBitcoin {
+  constructor(btcPreco, url) {
+    this.displayBtc = document.querySelector(btcPreco);
+    this.url = url;
+  }
+  async fetchBitcoin(url) {
     try {
       const responseBitcoin = await fetch(url);
       const jsonBitcoin = await responseBitcoin.json();
       const bitcoinPreco = jsonBitcoin.BRL.sell;
-      displayBtc.innerText = (100 / bitcoinPreco).toFixed(5);
+      this.displayBtc.innerText = (100 / bitcoinPreco).toFixed(5);
     } catch (erro) {
       console.log(Error(erro));
     }
   }
-  fetchBitcoin("https://blockchain.info/ticker");
+  init() {
+    if (this.displayBtc && this.url) {
+      this.fetchBitcoin(this.url);
+      return this;
+    }
+  }
 }
