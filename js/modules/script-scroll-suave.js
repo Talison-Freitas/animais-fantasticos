6637f31cc3ac900f9a982f40162c1e782c9c307b
Talison-Freitas <Talisondefreitas@outlook.com>
2025-01-23 00:40:14 -0300

    correção no módulo scroll-suave

--- js/modules/script-scroll-suave.js
+++ js/modules/script-scroll-suave.js
@@ -2,7 +2,7 @@ export default class ScrollSuave {
   constructor(links, options) {
     this.linksInternos = document.querySelectorAll(links);
     if (options === undefined) {
-      this.options = { top: "start", behavior: "smooth" };
+      this.options = { block: "start", behavior: "smooth" };
     } else {
       this.options = options;
     }
@@ -13,9 +13,7 @@ export default class ScrollSuave {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-    const sectionTop = section.offsetTop;
-    window.scrollTo(0, sectionTop);
-    window.scrollTo(this.options);
+    section.scrollIntoView(this.options);
   }
   addLinkEvent() {
     this.linksInternos.forEach((link) => {
