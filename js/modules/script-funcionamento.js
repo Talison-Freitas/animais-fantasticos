bc17393c47cae6cb1c04ba0ef34ce61e1ea5122d
Talison-Freitas <Talisondefreitas@outlook.com>
2025-01-26 22:57:03 -0300

    Refatora o módulo funcionamento, criando uma class principal para tornar o código mais limpo, otimizado e reutilizável.

--- js/modules/script-funcionamento.js
+++ js/modules/script-funcionamento.js
@@ -1,14 +1,36 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector("[data-semana]");
-  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
-  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
-  const dataAgora = new Date();
-  const diaAgora = dataAgora.getDay();
-  const horarioAgora = dataAgora.getHours();
-  const semanaAberta = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto =
-    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
-  if (semanaAberta && horarioAberto) {
-    funcionamento.classList.add("ativo");
+export default class Funcionamento {
+  constructor(diasSemana, activeClass) {
+    this.funcionamento = document.querySelector(diasSemana);
+    this.activeClass = activeClass;
+  }
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(",")
+      .map(Number);
+  }
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = (this.dataAgora.getUTCHours() - 3 + 24) % 24;
+  }
+  estaAberto() {
+    this.semanaAberta = this.diasSemana.indexOf(this.diaAgora) !== -1;
+    this.horarioAberto =
+      this.horarioAgora >= this.horarioSemana[0] &&
+      this.horarioAgora < this.horarioSemana[1];
+    return this.semanaAberta && this.horarioAberto;
+  }
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
   }
 }
